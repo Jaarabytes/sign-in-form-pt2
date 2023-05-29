76c0d78f016de3eedddd56359e86631dfa1c114c
Jaarabytes <128234882+Jaarabytes@users.noreply.github.com>
2023-05-29 21:32:56 +0300

    added the hide password feature

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
     $(document).on("click", "input", function() {
         $(this).css("border", "3px solid powderblue");
-      });         
+      }); 
+      
+        // Target the password input field
+  var passwordInput = $('#password');
+
+  // Hide the password when typing
+  passwordInput.on('input', function() {
+    var passwordValue = passwordInput.val();
+
+    // Replace the entered characters with asterisks
+    var hiddenPassword = '*'.repeat(passwordValue.length);
+
+    // Set the hidden password as the input value
+    passwordInput.val(hiddenPassword);
+  });
 })
